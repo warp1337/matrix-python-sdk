d45d361105eacd7ece40971520d325c1a970c0d7
Valentin Deniaud <valentin.deniaud@inpt.fr>
2018-06-29 10:59:24 +0200

    handle m.room.encryption properties

--- matrix_client/room.py
+++ matrix_client/room.py
@@ -47,6 +47,8 @@ class Room(object):
         self._prev_batch = None
         self._members = []
         self.encrypted = False
+        self.rotation_period_msgs = None
+        self.rotation_period_ms = None
 
     def set_user_profile(self,
                          displayname=None,
@@ -691,6 +693,10 @@ class Room(object):
             elif etype == "m.room.encryption":
                 if econtent.get("algorithm") == "m.megolm.v1.aes-sha2":
                     self.encrypted = True
+                    if not self.rotation_period_ms:
+                        self.rotation_period_ms = econtent.get("rotation_period_ms")
+                    if not self.rotation_period_msgs:
+                        self.rotation_period_msgs = econtent.get("rotation_period_msgs")
             elif etype == "m.room.member" and clevel == clevel.ALL:
                 # tracking room members can be large e.g. #matrix:matrix.org
                 user_id = state_event["state_key"]
